e77ce287f7aee52ebd4b78f627152d3af69c8f4f
Raffaele Sena <raff367@gmail.com>
2013-06-26 16:42:14 -0700

    A basic example

---
+++ example/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "../../cmd"
+
+    "fmt"
+    )
+
+func main() {
+    commander := &cmd.Cmd{}
+    commander.Init()
+
+    commander.Commands["ls"] = func(line string) (stop bool) {
+        fmt.Println("listing stuff")
+        return
+    }
+
+    commander.Commands["exit"] = func(line string) (stop bool) {
+        fmt.Println("goodbye!")
+        return true
+    }
+
+    commander.CmdLoop()
+}
