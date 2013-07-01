a42179752b137a6320659b516f1b4d41401cc631
Raffaele Sena <raff367@gmail.com>
2013-06-30 20:39:29 -0700

    More documentation

--- cmd.go
+++ cmd.go
@@ -1,6 +1,16 @@
 /*
  This package is used to implement "line oriented command interpreter", inspired by the python package with
  the same name http://docs.python.org/2/library/cmd.html
+ 
+ Usage:
+ 
+ commander := &Cmd{...}
+ commander.Init()
+ 
+ commander.Add(Command{...})
+ commander.Add(Command{...})
+ 
+ commander.CmdLoop()
  */
 package cmd
 
@@ -23,19 +33,40 @@ type Command struct {
 	Call func(string) bool
 }
 
+//
+// This the the "context" for the command interpreter
+//
 type Cmd struct {
+	// the prompt string
 	Prompt string
 
+	// this function is called before starting the command loop
 	PreLoop   func()
+	
+	// this function is called before terminating the command loop
 	PostLoop  func()
+	
+	// this function is called before executing the selected command
 	PreCmd    func(string)
+	
+	// this function is called after a command has been executed
+	// return true to terminate the interpreter, false to continue
 	PostCmd   func(string, bool) bool
+	
+	// this function is called if the last typed command was an empty line
 	EmptyLine func()
+	
+	// this function is called if the command line doesn't match any existing command
+	// by default it displays an error message
 	Default   func(string)
 
+	// this is the list of available commands indexed by command name
 	Commands map[string]Command
 }
 
+//
+// Initialize the command interpreter context
+//
 func (cmd *Cmd) Init() {
 	if cmd.PreLoop == nil {
 		cmd.PreLoop = func() {}
@@ -60,10 +91,18 @@ func (cmd *Cmd) Init() {
 	cmd.Add(Command{"help", `list available commands`, cmd.Help})
 }
 
+//
+// Add a command to the command interpreter.
+// Overrides a command with the same name, if there was one
+//
 func (cmd *Cmd) Add(command Command) {
 	cmd.Commands[command.Name] = command
 }
 
+//
+// Default help command.
+// It lists all available commands or it displays the help for the specified command
+//
 func (cmd *Cmd) Help(line string) (stop bool) {
 	if len(line) == 0 {
 		fmt.Println("Available commands:")
@@ -86,6 +125,9 @@ func (cmd *Cmd) Help(line string) (stop bool) {
 	return
 }
 
+//
+// This method executes one command
+//
 func (cmd *Cmd) OneCmd(line string) (stop bool) {
 
 	parts := strings.SplitN(line, " ", 2)
@@ -108,6 +150,10 @@ func (cmd *Cmd) OneCmd(line string) (stop bool) {
 	return
 }
 
+//
+// This is the command interpreter entry point.
+// It displays a prompt, waits for a command and executes it until the selected command returns true
+//
 func (cmd *Cmd) CmdLoop() {
 	if len(cmd.Prompt) == 0 {
 		cmd.Prompt = "> "
