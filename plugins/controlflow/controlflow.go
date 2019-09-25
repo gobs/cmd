84112735f679ea73251eb0a4e74c1f1b8bc9961b
Raffaele Sena <raff367@gmail.com>
2019-09-24 17:49:01 -0700

    Explain available "expr" operators

--- plugins/controlflow/controlflow.go
+++ plugins/controlflow/controlflow.go
@@ -1166,7 +1166,18 @@ func (cf *controlFlow) PluginInit(c *cmd.Cmd, ctx *internal.Context) error {
 	c.Add(cmd.Command{"var", `var [-g|--global|--parent] [-r|--remove|-u|--unset] name value`, cf.command_variable, nil})
 	c.Add(cmd.Command{"shift", `shift [n]`, cf.command_shift, nil})
 	c.Add(cmd.Command{"if", `if (condition) command`, cf.command_conditional, nil})
-	c.Add(cmd.Command{"expr", `expr operator operands...`, cf.command_expression, nil})
+	c.Add(cmd.Command{"expr", `expr operator operands...
+
+operators:
+  +|-|*|/ number number
+  round [up|down] number
+  rand max [base]
+  upper string
+  lower string
+  trim string
+  substr start:end string
+  re|regex|regexp expr string
+  or first rest`, cf.command_expression, nil})
 	c.Add(cmd.Command{"foreach", `foreach [--wait=duration] (items...) command`, cf.command_foreach, nil})
 	c.Add(cmd.Command{"repeat", `repeat [--count=n] [--wait=duration] [--echo] command`, cf.command_repeat, nil})
 	c.Add(cmd.Command{"load", `load script-file`, cf.command_load, nil})
